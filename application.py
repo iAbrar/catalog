3ad1f06b6cf58abb237290a9387765344301a1a4
Abrar <abrar.ksu@gmail.com>
2018-12-16 23:10:54 +0300

    change port number

--- application.py
+++ application.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-
+def HelloWorld():
+    return "Hello World"
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
