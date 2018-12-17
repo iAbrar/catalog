d1e6e9f2514cf119efb604fbb24cf4a9f5565d18
Abrar <abrar.ksu@gmail.com>
2018-12-18 01:04:42 +0300

    connect the app with the database

--- application.py
+++ application.py
@@ -1,5 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Category, Base, Item, User
+
+engine = create_engine('sqlite:///recipes.db?check_same_thread=False')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+st commit by calling
+# session.rollback()
+session = DBSession()
+
 
 # Show all categories
 @app.route('/')
