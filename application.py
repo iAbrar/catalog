a32043ef380ad13326ce19afb4ba42bdee9b1726
Abrar <abrar.ksu@gmail.com>
2018-12-24 21:45:08 +0300

    solve username issue

--- application.py
+++ application.py
@@ -104,7 +104,7 @@ def gconnect():
 
     data = answer.json()
 
-    login_session['username'] = data['email']
+    login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
     # ADD PROVIDER TO LOGIN SESSION
