a96c2ecb1df7b17199ea5f7991e1b9c6a4e96308
Abrar <abrar.ksu@gmail.com>
2018-12-29 00:04:56 +0300

    fix issue nutritions hasn't have an item.id

--- seeder.py
+++ seeder.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Category, Base, Item, User
+from database_setup import Category, Base, Item, User, Nutritions
 
 engine = create_engine('sqlite:///recipes.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -31,62 +31,102 @@ category1 = Category(user_id=1, title="Breakfast")
 session.add(category1)
 session.commit()
 
+
 item1 = Item(user_id=1, title="egg", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-             category=category1)
-
+            ingredients ="jhgfkdlkgnjhlf;ds'fmkgm;d'ldsfmkglf", instructions = "fdghffgfdsadfghjfhghfds",
+                        difficulty = "Easy", serves = 3,
+                        preparingTime = '11', cookingTime = '60',
+                       picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png',
+                      category=category1)
+item2 = Item(user_id=1, title="2", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+            ingredients ="jhgfkdlkgnjhlf;ds'fmkgm;d'ldsfmkglf", instructions = "fdghffgfdsadfghjfhghfds",
+                        difficulty = "Easy", serves = 3,
+                        preparingTime = '11', cookingTime = '60',
+                       picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png',
+                      category=category1)
 session.add(item1)
 session.commit()
-
-
-item2 = Item(user_id=1, title="Pan cake", description="with garlic and parmesan",
-             category=category1)
-
 session.add(item2)
 session.commit()
+nutritions1= Nutritions(item=item1,energy = '22',calories = '32',
+                    fat = '44',saturatedFat = '43',
+                    carbohydrate = '12',sugar = '0',
+                    dietaryFiber = '12',protein = '21',
+                    cholesterol = '33',sodium = '2')
+nutritions2= Nutritions(item=item2,energy = '22',calories = '32',
+                    fat = '44',saturatedFat = '43',
+                    carbohydrate = '12',sugar = '0',
+                    dietaryFiber = '12',protein = '21',
+                    cholesterol = '33',sodium = '2')
 
-item3 = Item(user_id=1, title="Shakshoka", description="with tomato and onion",
-             category=category1)
-
-session.add(item3)
+session.add(nutritions1)
+session.commit()
+session.add(nutritions2)
 session.commit()
 
-# Recipes for Lunch
-category2 = Category(user_id=1, title="Lunch")
+# item2 = Item(user_id=1, title="egg", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+#             ingredients ="jhgfkdlkgnjhlf;ds'fmkgm;d'ldsfmkglf", instructions = "fdghffgfdsadfghjfhghfds",
+#                         difficulty = "Easy", serves = 3,
+#                         preparingTime = '11', cookingTime = '60',
+#                        picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png',
+#              category=category1)
 
-session.add(category2)
-session.commit()
+# session.add(item2)
+# session.commit()
 
-item1 = Item(user_id=1, title="Qinoa Salad", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-             category=category2)
+# item3 = Item(user_id=1, title="egg", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+#             ingredients ="jhgfkdlkgnjhlf;ds'fmkgm;d'ldsfmkglf", instructions = "fdghffgfdsadfghjfhghfds",
+#                         difficulty = "Easy", serves = 3,
+#                         preparingTime = '11', cookingTime = '60',
+#                        picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png',
+#              category=category1)
 
-session.add(item1)
-session.commit()
+# session.add(item3)
+# session.commit()
+
+# # Recipes for Lunch
+# category2 = Category(user_id=1, title="Lunch")
+
+# session.add(category2)
+# session.commit()
+
+# item1 = Item(user_id=1, title="egg", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+#             ingredients ="jhgfkdlkgnjhlf;ds'fmkgm;d'ldsfmkglf", instructions = "fdghffgfdsadfghjfhghfds",
+#                         difficulty = "Easy", serves = 3,
+#                         preparingTime = '11', cookingTime = '60',
+#                         picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png',
+#              category=category2)
+
+# session.add(item1)
+# session.commit()
 
 
-item2 = Item(user_id=1, title="Green Salad", description="with garlic and parmesan",
-             category=category2)
+# item2 =Item(user_id=1, title="egg", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+#             ingredients ="jhgfkdlkgnjhlf;ds'fmkgm;d'ldsfmkglf", instructions = "fdghffgfdsadfghjfhghfds",
+#                         difficulty = "Easy", serves = 3,
+#                         preparingTime = '11', cookingTime = '60',
+#                         picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png',
+#              category=category2)
 
-session.add(item2)
-session.commit()
+# session.add(item2)
+# session.commit()
 
-# Recipes for Dinner
-category3 = Category(user_id=1, title="Dinner")
+# # Recipes for Dinner
+# category3 = Category(user_id=1, title="Dinner")
 
-session.add(category3)
-session.commit()
+# session.add(category3)
+# session.commit()
 
-item1 = Item(user_id=1, title="Beef Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-             category=category3)
+# item1 = Item(user_id=1, title="egg", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+#             ingredients ="jhgfkdlkgnjhlf;ds'fmkgm;d'ldsfmkglf", instructions = "fdghffgfdsadfghjfhghfds",
+#                         difficulty = "Easy", serves = 3,
+#                         preparingTime = '11', cookingTime = '60',
+#                         picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png',
+#              category=category3)
 
-session.add(item1)
-session.commit()
+# session.add(item1)
+# session.commit()
 
-
-item2 = Item(user_id=1, title="Grilled Salamon", description="with garlic and parmesan",
-             category=category3)
-
-session.add(item2)
-session.commit()
 
 
 print "added catalog items!"
