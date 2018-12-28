68a953bad94310a11cc01e3f72dd13137beab9b1
Abrar <abrar.ksu@gmail.com>
2018-12-28 05:07:09 +0300

    show only 4 items in showcategories

--- application.py
+++ application.py
@@ -155,18 +155,13 @@ def getUserID(email):
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
-        print 'Access Token is None'
         response = make_response(json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print 'In gdisconnect access token is %s', access_token
-    print 'User name is: '
-    print login_session['username']
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
+
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
@@ -194,9 +189,9 @@ def showCategoriesJSON():
 @app.route('/catalog/')
 def showCategories():
     categories = session.query(Category).all()
-    items = session.query(Item).all()
+    items = session.query(Item).limit(4)
     if 'username' not in login_session:
-        return render_template('publichome.html', categories=categories, items=items)
+        return render_template('index.html', categories=categories, items=items)
     else:
         return render_template('home.html', categories=categories, items=items)
 
