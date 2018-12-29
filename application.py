cc60a5310ce1342948160bc31c660e10e218ac88
Abrar <abrar.ksu@gmail.com>
2018-12-30 01:40:31 +0300

    applied pep8 style

--- application.py
+++ application.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import jsonify
+from flask import url_for
+from flask import flash
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Category, Base, Item, User, Nutritions
 
 from flask import session as login_session
-import random, string
+import random
+import string
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
@@ -34,11 +41,12 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return  render_template('login.html', STATE=state)
+    return render_template('login.html', STATE=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-     # Validate state token
+    # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -88,8 +96,8 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps(
+                                    'Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -122,7 +130,8 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ''' " style = "width: 300px; height: 300px;border-radius: 150px;
+                -webkit-border-radius: 150px;-moz-border-radius: 150px;"> '''
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
@@ -150,12 +159,12 @@ def getUserID(email):
         return None
 
 
-
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(json.dumps('Current user not connected.'),
+                                 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
@@ -174,15 +183,19 @@ def gdisconnect():
         return redirect(url_for('showCategories'))
 
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response = make_response(json.dumps(
+                                 'Failed to revoke token for given user.',
+                                 400))
         response.headers['Content-Type'] = 'application/json'
         return response
+
 
 # JSON APIs to view the catalog
 @app.route('/catalog/JSON')
 def showCategoriesJSON():
     categories = session.query(Category).all()
     return jsonify(categories=[i.serialize for i in categories])
+
 
 # JSON Show all recipes for one category
 @app.route('/catalog/<int:category_id>/recipes/JSON')
@@ -191,9 +204,10 @@ def showRecipesJSON(category_id):
     items = session.query(Item).filter_by(category_id=category_id).all()
     return jsonify(items=[i.serialize for i in items])
 
+
 # JSON Show recipe information
 @app.route('/catalog/<int:category_id>/recipes/<int:recipe_id>/')
-def showrecipeJSON(category_id,recipe_id):
+def showrecipeJSON(category_id, recipe_id):
     category = session.query(Category).filter_by(id=category_id).one()
     item = session.query(Item).filter_by(id=recipe_id).one()
     return jsonify(recipe=item.serialize)
@@ -210,9 +224,11 @@ def showCategories():
     for i in items:
         print(i)
     if 'username' not in login_session:
-        return render_template('index.html', categories=categories, items=items)
+        return render_template('index.html', categories=categories,
+                               items=items)
     else:
         return render_template('home.html', categories=categories, items=items)
+
 
 # Show all recipes
 @app.route('/catalog/<int:category_id>/recipes/')
@@ -220,7 +236,7 @@ def showRecipes(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(category_id=category_id).all()
 
-     # This code to show specific imaage for each category
+# This code to show specific imaage for each category
     if category_id == 1:
         img = url_for('static', filename='images/work-image1.jpg')
     elif category_id == 2:
@@ -229,13 +245,16 @@ def showRecipes(category_id):
         img = url_for('static', filename='images/work-image3.jpg')
     # code end #
     if 'username' not in login_session:
-        return render_template('publiccategory.html', category=category, items=items, img=img)
+        return render_template('publiccategory.html',
+                               category=category, items=items, img=img)
     else:
-        return render_template('category.html', category=category, items=items, img=img)
+        return render_template('category.html', category=category,
+                               items=items, img=img)
+
 
 # Show one recipe
 @app.route('/catalog/<int:category_id>/recipes/<int:recipe_id>/')
-def showrecipe(category_id,recipe_id):
+def showrecipe(category_id, recipe_id):
     category = session.query(Category).filter_by(id=category_id).one()
     item = session.query(Item).filter_by(id=recipe_id).one()
     # sprate their values in list to show the later in <li>
@@ -244,46 +263,69 @@ def showrecipe(category_id,recipe_id):
     instructions = item.instructions
     instructions = instructions.split("\n")
     if 'username' not in login_session:
-        return render_template('publicrecipe.html', category=category, item=item,ingredients=ingredients,instructions=instructions)
+        return render_template('publicrecipe.html', category=category,
+                               item=item, ingredients=ingredients,
+                               instructions=instructions)
     else:
-        return render_template('recipe.html', category=category, item=item,ingredients=ingredients,instructions=instructions)
+        return render_template('recipe.html', category=category,
+                               item=item, ingredients=ingredients,
+                               instructions=instructions)
+
+
 # create a recipe
-@app.route('/catalog/<int:category_id>/recipes/new/',methods=['GET','POST'])
+@app.route('/catalog/<int:category_id>/recipes/new/', methods=['GET', 'POST'])
 def newrecipe(category_id):
     if 'username' not in login_session:
         return redirect('/login')
 
     if request.method == 'POST':
-        nutritions1 = [Nutritions(energy = request.form['energy'],calories = request.form['calories'],
-                    fat = request.form['fat'],saturatedFat = request.form['s-fat'],
-                    carbohydrate = request.form['carbs'],sugar = request.form['sugar'],
-                    dietaryFiber = request.form['fibers'],protein = request.form['protein'],
-                    cholesterol = request.form['chol'],sodium = request.form['sodium'])]
+        nutritions1 = [Nutritions(energy=request.form['energy'],
+                                  calories=request.form['calories'],
+                                  fat=request.form['fat'],
+                                  saturatedFat=request.form['s-fat'],
+                                  carbohydrate=request.form['carbs'],
+                                  sugar=request.form['sugar'],
+                                  dietaryFiber=request.form['fibers'],
+                                  protein=request.form['protein'],
+                                  cholesterol=request.form['chol'],
+                                  sodium=request.form['sodium'])]
 
-        newItem = Item(title = request.form['name'],description = request.form['description'],
-                        ingredients = request.form['ingredients'], instructions = request.form['instructions'],
-                        difficulty = request.form['difficulty'], serves = request.form['serves'],
-                        preparingTime = request.form['p-time'], cookingTime = request.form['c-time'],
-                        nutritions=nutritions1,picture=request.form['image'],
-                        category_id=category_id,  user_id=login_session['user_id'])
+        newItem = Item(title=request.form['name'],
+                       description=request.form['description'],
+                       ingredients=request.form['ingredients'],
+                       instructions=request.form['instructions'],
+                       difficulty=request.form['difficulty'],
+                       serves=request.form['serves'],
+                       preparingTime=request.form['p-time'],
+                       cookingTime=request.form['c-time'],
+                       nutritions=nutritions1,
+                       picture=request.form['image'],
+                       category_id=category_id,
+                       user_id=login_session['user_id'])
         session.add(newItem)
         session.commit()
-        flash("new  recipe %s created!" %(newItem.title))
-        return redirect(url_for('showRecipes',category_id=category_id))
+        flash("new  recipe %s created!" % (newItem.title))
+        return redirect(url_for('showRecipes', category_id=category_id))
     else:
         return render_template('new.html', category_id=category_id)
 
+
 # edit specific recipe
-@app.route('/catalog/<int:category_id>/recipe/<int:recipe_id>/edit/',methods=['GET','POST'])
-def editrecipe(category_id,recipe_id):
+@app.route('/catalog/<int:category_id>/recipe/<int:recipe_id>/edit/',
+           methods=['GET', 'POST'])
+def editrecipe(category_id, recipe_id):
     editedItem = session.query(Item).filter_by(id=recipe_id).one()
-    nutritions = session.query(Nutritions).filter_by(item_id=editedItem.id).one()
+    nutritions = session.query(Nutritions).filter_by(
+                               item_id=editedItem.id).one()
 
     if 'username' not in login_session:
         return redirect('/login')
     if editedItem.user_id != login_session['user_id']:
-        return """<script>function myFunction() {alert('You are not authorized to edit this recipe. Please create your own recipe in order to edit.');
-         window.location.href = '/catalog/"""+str(category_id)+"/recipes/';}</script><body onload='myFunction()''>"""
+        return '''<script>function myFunction() {
+        alert('You are not authorized to edit this recipe.
+        Please create your own recipe in order to edit.');
+         window.location.href = '/catalog/"""+str(category_id)+"/recipes/';}
+         </script><body onload='myFunction()''>'''
         return redirect(url_for('showCategories'))
     if request.method == 'POST':
         if request.form['title']:
@@ -329,19 +371,28 @@ def editrecipe(category_id,recipe_id):
 
         session.add(editedItem)
         session.commit()
-        flash("recipe %s Successfully Edited!" %(editedItem.title))
-        return redirect(url_for('showrecipe',category_id=editedItem.category_id,recipe_id=editedItem.id))
+        flash("recipe %s Successfully Edited!" % (editedItem.title))
+        return redirect(url_for('showrecipe',
+                        category_id=editedItem.category_id,
+                        recipe_id=editedItem.id))
     else:
-        return render_template('edit.html', category_id=category_id, recipe_id=recipe_id, item =editedItem)
+        return render_template('edit.html', category_id=category_id,
+                               recipe_id=recipe_id, item=editedItem)
+
 
 # delete specific recipe
-@app.route('/catalog/<int:category_id>/recipe/<int:recipe_id>/delete/', methods =['GET','POST'])
-def deleterecipe(category_id,recipe_id):
+@app.route('/catalog/<int:category_id>/recipe/<int:recipe_id>/delete/',
+           methods=['GET', 'POST'])
+def deleterecipe(category_id, recipe_id):
     deletedItem = session.query(Item).filter_by(id=recipe_id).one()
     if 'username' not in login_session:
         return redirect('/login')
     if deletedItem.user_id != login_session['user_id']:
-        return "<script>function myFunction() {alert('You are not authorized to delete this recipe. Please create your own recipe in order to delete.');window.location.href = '/catalog/"""+str(category_id)+"/recipes/'}</script><body onload='myFunction()''>"
+        return '''<script>function myFunction() {
+            alert('You are not authorized to delete this recipe. Please create
+            your own recipe in order to delete.');window.location.href =
+            '/catalog/"""+str(category_id)+"/recipes/'}</script>
+            <body onload='myFunction()''>'''
     if request.method == 'POST':
         session.delete(deletedItem)
         session.commit()
